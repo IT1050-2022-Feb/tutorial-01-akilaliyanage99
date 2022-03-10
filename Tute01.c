e9e1ae94b4bbba67a4dd4f2e7179e9e1010f1d64
IT21180484 <90019916+IT21180484@users.noreply.github.com>
2022-03-10 08:18:18 +0000

    finished

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark1,mark2;
+  float average;
+  printf("enter 1st subject mark =");
+  scanf("%d",&mark1);
+  printf("enter 2nd subject mark =");
+  scanf("%d",&mark2);
+
+  average=(mark1+mark2)/2.0;
+
+  printf("your answer is =%.2f",average);
+
+
   
   return 0;
 }
